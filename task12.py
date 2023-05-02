6dac6e3406242576d97174f550709cad4def93b1
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-05-02 20:40:36 +0600

    Update task12.py

--- task12.py
+++ task12.py
@@ -3,4 +3,4 @@ P = int(input("Введите произведение чисел: "))
   for x in range(S):
     for y in range(P):
       if S == x + y and P == x * y:
-print(f"Числа задуманные Петей :{x, y}")
+        print(f"Числа задуманные Петей :{x, y}")
