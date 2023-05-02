5e4df9725085ef3338083194124fb4535c5c75be
StoneColdSteveAustine <115403281+StoneColdSteveAustine@users.noreply.github.com>
2023-05-02 20:39:23 +0600

    Update task10.py

--- task10.py
+++ task10.py
@@ -2,12 +2,12 @@ amount_coin = int(input("Введите количество монет :"))
 count_head = 0
 count_tail = 0
   for i in range(amount_coin):
-  x = int(input())
-    if x == 0:
-    count_head += 1
-    else:
-    count_tail += 1
-if count_head > count_tail:
-print(f"Минимальное количество монет, которые нужно перевернуть -"{count_tail})
-else:
-print(f"Минимальное количество монет, которые нужно перевернуть -"{count_head})
+    x = int(input())
+      if x == 0:
+        count_head += 1
+      else:
+        count_tail += 1
+          if count_head > count_tail:
+            print(f"Минимальное количество монет, которые нужно перевернуть -"{count_tail})
+          else:
+            print(f"Минимальное количество монет, которые нужно перевернуть -"{count_head})
